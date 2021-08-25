045cb64d2d4674a6390e6b9e054439ebc382ebf9
조경진 <cho881020@gmail.com>
2021-08-25 14:58:44 +0900

    가게 데이터를 lateinit var 변수에 담아서 실제 값 보여주기

--- app/src/main/java/com/neppplus/pizzaorderapp_20210825/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210825/ViewStoreDetailActivity.kt
@@ -2,8 +2,13 @@ package com.neppplus.pizzaorderapp_20210825
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.neppplus.pizzaorderapp_20210825.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
+
+    lateinit var mStoreData: StoreData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +19,18 @@ class ViewStoreDetailActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+
     }
 
     override fun setValues() {
+
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+
+        nameTxt.text = mStoreData.name
+        phoneNumTxt.text = mStoreData.phoneNum
+
+        Glide.with(mContext).load(mStoreData.logoURL).into(logoImg)
+
 
     }
 
